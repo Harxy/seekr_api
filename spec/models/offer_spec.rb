995b939b9f4ec91820c6a5f12274712bad318b77
Robert Morgan <robbie.morgan@gmail.com>
2015-08-18 10:36:58 +0100

    Added unit tests for offers

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,5 +1,6 @@
-# require 'rails_helper'
-#
-# RSpec.describe Offer, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+describe Offer, type: :model do
+  it { is_expected.to belong_to :jobseeker }
+  it { is_expected.to belong_to :job }
+end
