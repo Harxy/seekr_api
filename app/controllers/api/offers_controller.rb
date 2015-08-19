720b5bf9737625d8563f8265c9303bc6f3db8894
Paul Harker <harkerp@gmail.com>
2015-08-19 12:33:46 +0100

    ptolomy test

--- app/controllers/api/offers_controller.rb
+++ app/controllers/api/offers_controller.rb
@@ -1,52 +1,44 @@
 module Api
-  class OffersController < ApplicationController
-    def create
-      offer = Offer.new(offer_params)
-      if offer.save
-        render json: { status: 'offer successfully created', offer_id: offer.id }.to_json
-      else
-        render json: { status: 'failed', error: offer.errors.first }.to_json
+    class OffersController < ApplicationController
+      def create
+        offer = Offer.new(offer_params)
+        if offer.save
+          render json: { status: 'offer successfully created', offer_id: offer.id }.to_json
+        else
+          render json: { status: 'failed', error: offer.errors.first }.to_json
+        end
+      end
+
+      def job_offers
+        job = Job.find(params[:id])
+        job_offers = Offer.find_by(job_id: job.id, accepted: true)
+        render json: job_offers
+      end
+
+      def return_all
+        jobseekers = Jobseeker.all.reject do |job_seeker|
+          job_seeker.offers.empty?
+        end
+        render json: jobseekers
+      end
+
+      # def return_all
+      #    offers = Offer.where({job_id: params[:id]})
+      #    array = []
+      #    offers.each do |x|
+      #      array << x.jobseeker_id
+      #    end
+      #    render json: array
+      # end
+
+      private
+
+      def offer_params
+        params.permit(:job_id, :jobseeker_id, :accepted)
+      end
+
+      def offer_all_params
+        params.permit(:job_id)
       end
     end
-
-    def job_offers
-      job = Job.find(params[:id])
-      job_offers = Offer.find_by(job_id: job.id, accepted: true)
-      render json: job_offers
-    end
-
-    def return_all
-      require 'byebug'
-      byebug
-      jsarray = []
-      jobseekers = Jobseeker.all
-      jobseekers.each do |x|
-        jsarray << x
-      end
-      offers = Offer.where(job_id: params[:id])
-      offers.each do |x|
-        jsarray.delete x.jobseeker_id
-      end
-      render json: jsarray
-    end
-
-    # def return_all
-    #    offers = Offer.where({job_id: params[:id]})
-    #    array = []
-    #    offers.each do |x|
-    #      array << x.jobseeker_id
-    #    end
-    #    render json: array
-    # end
-
-    private
-
-    def offer_params
-      params.permit(:job_id, :jobseeker_id, :accepted)
-    end
-
-    def offer_all_params
-      params.permit(:job_id)
-    end
-  end
 end
--- spec/api/seekrs_spec.rb
+++ spec/api/seekrs_spec.rb
@@ -105,11 +105,10 @@ describe 'can ask for jobseekers' do
     expect(JSON.parse(last_response.body)).to eq [jobseeker1.id, jobseeker2.id]
   end
 
-  it 'that are accepted only' do
+  it 'that have not yet been evaluated for the job' do
     job = create :job
     jobseeker1 = create :jobseeker
-    jobseeker2 = create :jobseeker
-    get 'api/offers/all/1'
+
     post 'api/offers', {job_id: job.id.to_s, jobseeker_id: jobseeker1.id.to_s, accepted: true}.to_json, 'CONTENT_TYPE' => 'application/json'
     post 'api/offers/accepted', {job_id: job.id.to_s, accepted: true}.to_json, 'CONTENT_TYPE' => 'application/json'
     expect(JSON.parse(last_response.body)).to eq [jobseeker1.id]
