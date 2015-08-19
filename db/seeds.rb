b6e122db72167078aaedaf2ba6d4b0b2d2cc9347
Paul Harker <harkerp@gmail.com>
2015-08-19 10:23:41 +0100

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Jobseeker.create(name:"Paul", email:"paul@gmail.com", location:"coolville", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/paulimg.png', PrimarySkill: 'Writing', SecondarySkill: 'Bartending')
-Jobseeker.create(name:"Rob", email:"rob@seekr.com", location:"lovelyville", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/robimg.jpeg', PrimarySkill: 'Customer Service', SecondarySkill: 'Maths')
-Jobseeker.create(name:"Jack", email:"jack@seekr.com", location:"lovelyville", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/jackimg.png', PrimarySkill: 'Babysitting', SecondarySkill: 'Woodworking')
+Jobseeker.create(name:"Rob", email:"rob@seekr.com", location:"lovelyville", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/robimg.jpeg', PrimarySkill: 'Customer Service', SecondarySkill: 'Writing')
+Jobseeker.create(name:"Jack", email:"jack@seekr.com", location:"lovelyville", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/jackimg.png', PrimarySkill: 'Babysitting', SecondarySkill: 'Security')
 Jobseeker.create(name:"Nikesh", email:"nikesh@seekr.com", location:"lovelyville", imageURL: 'https://s3-eu-west-1.amazonaws.com/makers-seekr/nikeshimg.jpeg', PrimarySkill: 'Moaning', SecondarySkill: 'Dog walking')
 Jobseeker.create(name:"Simon", email:"simon@seekr.com", location:"lovelyville", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/simonimg.jpeg', PrimarySkill: 'Security', SecondarySkill: 'Customer Service')
 Jobseeker.create(name:"Ptolemy", email:"ptolemy@seekr.com", location:"lovelyville", imageURL: 'https://s3-eu-west-1.amazonaws.com/makers-seekr/ptolemyimg.jpeg', PrimarySkill: 'Developer', SecondarySkill: 'Security')
@@ -17,7 +17,9 @@ Jobseeker.create(name:"Christian", email:"christian@seekr.com", location:"lovely
 Employer.create(name:"Makers", description:"We love testing!", industry:"making developers who test", website:"www.makersacademy.com", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/makersacademylogo.png')
 Employer.create(name:"General Assembly", description:"We train seals to hit keyboards", industry:"flooding the market with poor programmers", website:"www.wecantmakeawebsite.clom", imageURL:'https://s3-eu-west-1.amazonaws.com/makers-seekr/generalassemblylogo.png')
 
-Job.create(title: 'Dream Maker', description: 'We need a Ptolemy! ', start_date: '14/09/2015', duration: '30 weeks', hours: '45', location: 'London', wage: 8.97, requiredSkills: "Typing")
+Job.create(title: 'Dream Maker', description: 'We need a Ptolemy! ', start_date: '14/09/2015', duration: '30 weeks', hours: '45', location: 'London', wage: 8.97, requiredSkills: "Typing", employer_id: 1)
+Job.create(title: 'Dream Breaker', description: 'Someone to show Makers the harsh reality of life', start_date: '14/09/2015', duration: '30 weeks', hours: '45', location: 'London', wage: 8.97, requiredSkills: "Writing", employer_id: 1)
+Job.create(title: 'Zoo Keeper', description: 'Someone to feed the seals', start_date: '14/09/2015', duration: '30 weeks', hours: '45', location: 'London', wage: 8.97, requiredSkills: "Security" employer_id: 2)
 
 
 Offer.create(job_id: 1, jobseeker_id: 1, accepted: true)
