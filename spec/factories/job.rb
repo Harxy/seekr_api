b335d57f6c42045387b261ea617519c912cc7e33
Paul Harker <harkerp@gmail.com>
2015-08-19 22:04:41 +0100

    heroku fix

--- spec/factories/job.rb
+++ spec/factories/job.rb
@@ -8,7 +8,9 @@ FactoryGirl.define do
     hours "7"
     location "a bit too close"
     wage 8.90
-    requiredSkills "Typing  "
+    requiredSkills "Typing"
+    contactName "Simon"
+    contactEmail "simon@seekr.com"
     employer
   end
 end
